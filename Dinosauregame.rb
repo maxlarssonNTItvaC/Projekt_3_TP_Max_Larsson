1efd1ac69664b105d3d338fb788cd975c44fa8d8
maxlarssonNTItvaC <150041214+maxlarssonNTItvaC@users.noreply.github.com>
2024-04-26 10:27:24 +0200

    background

--- Dinosauregame.rb
+++ Dinosauregame.rb
@@ -1,14 +1,23 @@
 require 'ruby2d'
 
 set title: "mqueenrun"
-
 set fullscreen: true
+
+backGround1 = Sprite.new('img/dessertbackground.jpg')
+backGround2 = Sprite.new('img/dessertbackground.jpg', x:800)
 
 hero = Sprite.new('img/mqueen.png',
 width: 30,
 height: 86,
-clip_width: 30
+clip_width: 30,
+y: 320
 )
 
+
+update do
+    backGround1.x -= 3
+    backGround2.x -= 3
+
+end
 
 show
